4b31de12bd1a8a92234f81a2d51f487668c45aba
Hrulekh <97230911+Hrulekh@users.noreply.github.com>
2025-02-18 19:15:59 +0530

    data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,24 +4,22 @@ from sklearn.preprocessing import LabelEncoder
 def load_and_preprocess_data(file_path):
     df = pd.read_csv(file_path)
 
-    # Drop unnecessary columns (like Customer ID if present)
+   
     if 'customerID' in df.columns:
         df.drop(columns=['customerID'], inplace=True)
 
-    # Convert 'TotalCharges' to numeric (handling errors)
+    
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-    df.fillna(0, inplace=True)  # Fill missing values
+    df.fillna(0, inplace=True)  
 
-    # Convert categorical columns to numeric using Label Encoding
     label_encoders = {}
     for col in df.select_dtypes(include=['object']).columns:
         le = LabelEncoder()
         df[col] = le.fit_transform(df[col])
-        label_encoders[col] = le  # Store encoders if needed later
-
+        label_encoders[col] = le  
     return df, label_encoders
 
 if __name__ == "__main__":
     df, label_encoders = load_and_preprocess_data("WA_Fn-UseC_-Telco-Customer-Churn.csv")
-    df.to_csv("processed_churn.csv", index=False)  # Save cleaned data
-    print(df.head())  # Check output
+    df.to_csv("processed_churn.csv", index=False)  
+    print(df.head())
