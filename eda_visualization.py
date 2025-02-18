dd6f3d90db554f78d7fb237be7ce4e70d8b86b1a
Hrulekh <97230911+Hrulekh@users.noreply.github.com>
2025-02-18 19:16:35 +0530

    eda_visualization.py

--- eda_visualization.py
+++ eda_visualization.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def visualize_data(df):
-    # Count plot for churn
+ 
     plt.figure(figsize=(6, 4))
     sns.countplot(x='Churn', data=df)
     plt.title("Churn Distribution")
     plt.show()
 
-    # Heatmap to check feature correlation
+   
     plt.figure(figsize=(12, 6))
     sns.heatmap(df.corr(), annot=True, cmap="coolwarm", fmt=".2f")
     plt.title("Feature Correlation Heatmap")
     plt.show()
 
 if __name__ == "__main__":
-    df = pd.read_csv("processed_churn.csv")  # Load dataset
+    df = pd.read_csv("processed_churn.csv")  
     visualize_data(df)
